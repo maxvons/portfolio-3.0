75a9f251569fd2dc288512e4c1f563c1abccc287
Maximilian von Stephanides <maximilianvons@gmail.com>
2025-01-15 21:57:24 +0100

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Maximilian von Stephanides";
+export const SITE_DESCRIPTION =
+  "The personal website of software engineer and powerlifter Maximilian von Stephanides";
